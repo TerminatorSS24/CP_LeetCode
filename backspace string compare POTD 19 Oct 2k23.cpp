f9a4ca589504330d1e1dc3790cf30db69ddd7ced
Subhraneel Saha <125677043+TerminatorSS24@users.noreply.github.com>
2023-10-19 08:07:27 +0530

    POTD 19 OCT 2023

---
+++ backspace string compare POTD 19 Oct 2k23.cpp
@@ -0,0 +1,67 @@
+/*Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+**********LEETCODE POTD(19/10/2023)**********
+*/
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+class Solution
+{
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        stack<char> s1, s2;
+        for (char c : s)
+        {
+            if (c != '#')
+                s1.push(c);
+            else
+            {
+                if (s1.empty())
+                    continue;
+                s1.pop();
+            }
+        }
+        for (char c : t)
+        {
+            if (c != '#')
+                s2.push(c);
+            else
+            {
+                if (s2.empty())
+                    continue;
+                s2.pop();
+            }
+        }
+        while (!s1.empty() && !s2.empty())
+        {
+            if (s1.top() != s2.top())
+                return false;
+            s1.pop();
+            s2.pop();
+        }
+        if (!s1.empty() || !s2.empty())
+            return false;
+        return true;
+    }
+};
