d7031c5d4a83dcfe81b121127296a4df799b47d5
Subhraneel Saha <125677043+TerminatorSS24@users.noreply.github.com>
2024-03-06 08:20:51 +0530

    POTD 05 MARCH 2024

---
+++ linked list cycle POTD 5 Msrch 2k24.c
@@ -0,0 +1,65 @@
+/*Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+
+Example 1:
+
+image: https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist.png
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+image: https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test2.png
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+image: https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test3.png
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+bool hasCycle(struct ListNode *head)
+{
+    struct ListNode *slow = head, *fast = NULL;
+    if (!head || !head->next)
+    {
+        return false;
+    }
+    fast = head->next;
+    while (slow != fast)
+    {
+        if (!fast)
+        {
+            return false;
+        }
+        slow = slow->next;
+        if (fast->next && fast->next->next)
+        {
+            fast = fast->next->next;
+        }
+        else
+        {
+            fast = fast->next;
+        }
+    }
+    return true;
+}
