4d5d277a00034c603f6cac8d260f41a0efb21678
Subhraneel Saha <125677043+TerminatorSS24@users.noreply.github.com>
2024-03-11 08:01:58 +0530

    POTD 11 MARCH 2024

---
+++ custom sort string POTD 11 March 2k24.c
@@ -0,0 +1,47 @@
+/*You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+
+Return any permutation of s that satisfies this property.
+
+
+
+Example 1:
+
+Input:  order = "cba", s = "abcd"
+
+Output:  "cbad"
+
+Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
+
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+
+Example 2:
+
+Input:  order = "bcafg", s = "abcd"
+
+Output:  "bcad"
+
+Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
+
+Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", "a" maintain their order.
+
+**********LEETCODE POTD(11/03/2024)**********
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int *table;
+int comp(const void *a, const void *b)
+{
+    return table[(*(char *)a) - 'a'] - table[(*(char *)b) - 'a'];
+}
+char *customSortString(char *order, char *s)
+{
+    table = malloc(26 * sizeof(int));
+    for (int i = 0; i < strlen(order); i++)
+        table[order[i] - 'a'] = i;
+    qsort(s, strlen(s), sizeof(s[0]), comp);
+    free(table);
+    return s;
+}
